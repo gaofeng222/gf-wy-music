4e8e8b7c5a60a6cf1b911e499f648a200f05c954
gaofeng222 <596642721@qq.com>
2025-05-15 21:41:06 +0800

    add:基础配置

---
+++ craco.config.js
@@ -0,0 +1,24 @@
+/* craco.config.js */
+const path = require('path')
+module.exports = {
+  // 配置别名
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@a': path.resolve(__dirname, 'src/assets'),
+      '@c': path.resolve(__dirname, 'src/components'),
+      '@v': path.resolve(__dirname, 'src/views')
+    },
+    // 省略文件扩展名s
+    configure: {
+      resolve: {
+        extensions: ['.js', '.jsx', '.json', '.css', 'tsx']
+      }
+    }
+  },
+  style: {
+    postcss: {
+      //   plugins: [require('tailwindcss'), require('autoprefixer')]
+    }
+  }
+}
